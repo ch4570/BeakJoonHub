8efe0567d297375f5abb97e7281217f0fb40ac51
DevSeoRex <ckdekrn88@gmail.com>
2023-03-06 13:09:38 +0900

    [Bronze V] Title: A+B - 5, Time: 128 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		while(true) {
+			String[] arr = br.readLine().split(" ");
+			int a = Integer.parseInt(arr[0]);
+			int b = Integer.parseInt(arr[1]);
+
+			if(a==0 && b==0) {
+				break;
+			}else {
+				bw.write(String.valueOf((a+b)));
+				bw.newLine();
+				bw.flush();
+			}
+		}
+	}
+}
