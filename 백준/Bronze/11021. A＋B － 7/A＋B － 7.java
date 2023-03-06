a6b7f6291305ce7c4b25305764b7d99ef1910475
DevSeoRex <ckdekrn88@gmail.com>
2023-03-06 13:09:54 +0900

    [Bronze V] Title: A+B - 7, Time: 160 ms, Memory: 16096 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int a = Integer.parseInt(br.readLine());
+		for(int i=0; i<a; i++) {
+			String[] arr = br.readLine().split(" ");
+			int c = Integer.parseInt(arr[0]);
+			int d = Integer.parseInt(arr[1]);
+			bw.write("Case #"+(i+1)+": "+(c+d)+"\n");
+		}
+		bw.flush();
+
+	}
+
+}
