d4717a509d4f41cf578cb2f8aa0def5617b975c2
DevSeoRex <ckdekrn88@gmail.com>
2023-03-06 13:10:10 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String a = sc.next();
+    char b = a.charAt(0);
+    
+    int d = b;
+    
+    System.out.println(d);
+}
+
+}
