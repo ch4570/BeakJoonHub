f2910260aa8963e05208c769a4454de140293050
DevSeoRex <ckdekrn88@gmail.com>
2023-08-15 14:21:17 +0900

    [Silver I] Title: 미로 탐색, Time: 140 ms, Memory: 14760 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,8 +1,5 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -19,7 +16,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         input();
         BFS();
-        System.out.println(map[N][M]);
+        output();
     }
 
     static void BFS() {
@@ -61,6 +58,11 @@ public class Main {
                 map[i][j] = line.charAt(j - 1) - 48;
             }
         }
+    }
 
+    static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(map[N][M]));
+        bw.flush();
     }
 }
