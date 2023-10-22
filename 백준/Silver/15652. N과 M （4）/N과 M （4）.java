2b165b93851f7be8eb092de27a93c7b21b7cdb7f
DevSeoRex <ckdekrn88@gmail.com>
2023-10-22 16:56:58 +0900

    [Silver III] Title: N과 M (4), Time: 160 ms, Memory: 17364 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,53 @@
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder builder = new StringBuilder();
+    static int N, M;
+    static int[] answer;
+
+
+    public static void main(String[] args) throws IOException {
+        input();
+        DFS(0);
+        output();
+    }
+
+
+    private static void DFS(int L) {
+        if (L == M) {
+            for (int number : answer) builder.append(number).append(" ");
+            builder.append("\n");
+            return;
+        }
+
+        for (int i=1; i<=N; i++) {
+            if (L == 0) {
+                answer[L] = i;
+                DFS(L + 1);
+            } else {
+                if (L - 1 >= 0 && answer[L-1] <= i) {
+                    answer[L] = i;
+                    DFS(L + 1);
+                }
+            }
+        }
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
+        answer = new int[M];
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(builder.toString());
+        bw.flush();
+    }
+}
